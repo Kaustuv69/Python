d99ea79c3dcc1b8ddc70521529f3a73980614c55
Kaustuv69 <142577936+Kaustuv69@users.noreply.github.com>
2023-08-20 07:07:33 +0545

    Update report card.py

--- report card.py
+++ report card.py
@@ -16,7 +16,7 @@ s = x + b + c + d + e + f + g
 if s<=700:
     break
 else:
-    print"Reenter the marks properly each subject contains 100 marks"
+    print("Reenter the marks properly each subject contains 100 marks")
 ts = 700
 print("Obtained marks:", s)
 print("Total marks :", 700)
